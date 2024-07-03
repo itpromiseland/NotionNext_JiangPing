06249bd3cc296383960cb1354fc3482647133caf
itpromiseland <61225865+itpromiseland@users.noreply.github.com>
2024-07-03 18:25:09 +0800

    Update config.js

--- themes/simple/config.js
+++ themes/simple/config.js
@@ -3,7 +3,7 @@ const CONFIG = {
   SIMPLE_LOGO_IMG: '/Logo.webp',
   SIMPLE_TOP_BAR: true, // 显示顶栏
   SIMPLE_TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  SIMPLE_LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>姜萍粉丝<br/>/非姜萍本人<br/>/记录姜萍的传奇经历</div>',
+  SIMPLE_LOGO_DESCRIPTION: '<div>姜萍粉丝<br/>/非姜萍本人<br/>/记录姜萍的传奇经历</div>',// process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || 
 
   SIMPLE_AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
